b3040d3101c8784d59443a374454b9bc58bfcfdf
Felix Joeken <felix@joeken.eu>
2019-04-06 00:17:14 +0200

    changed the ammount of the tnts to 10

--- TntOnDeath/src/Main.java
+++ TntOnDeath/src/Main.java
@@ -1,9 +1,13 @@
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 
 public class Main extends JavaPlugin {
 
@@ -13,10 +17,11 @@ public class Main extends JavaPlugin {
 
             @EventHandler
             public void spielerStirbt(PlayerDeathEvent event) {
-                event.getEntity().getWorld().getBlockAt(event.getEntity().getLocation()).setType(Material.TNT);
+                for (int i = 0; i < 10; i++) {
+                    Entity entity = event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), EntityType.PRIMED_TNT);
+                    entity.setVelocity(new Vector(5*(Math.random()-0.5f), 5*(Math.random()-0.5f),5*(Math.random()-0.5f)));
+                }
             }
-
-
         }, this);
     }
 }
